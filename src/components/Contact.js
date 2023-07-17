8ca9afb63919cf625a11299ef69e39f53feddca7
whiskacz <pawelblasiak1985@gmail.com>
2023-07-17 18:44:52 +0200

    exercise done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import '../css/Contact.css'
 
-export default function Contact( {Data}) {
+export default function Contact( {contact}) {
   return (
-    <div key={Data.id} className='contactWrapper'>
-        <div className='mainName'>{Data.user_name}</div>
-       <div>Last name:</div> 
-       <div>Email:</div> 
-       <div>Phone: </div>
-       <div>User name: </div>
+    <div key={contact.id} className='contactWrapper'>
+        <div className='mainName'>{contact.first_name}</div>
+        <div>Last name: {contact.last_name}</div> 
+        <div>Email:{contact.email}</div> 
+        <div>Phone: {contact.phone}</div>
+        <div>User name: {contact.user_name}</div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState} from "react";
 import ReactDOM from 'react-dom/client';
 import Contact from "./components/Contact";
 import './css/index.css'
 import Data from './components/Data.json'
 
 export default function R14App() {
+    
+    const [text, setText] = useState("")
+
+    const handleText = (e) => {
+        setText(e.target.value)
+    }
+    
     return(
     <div className="wrapper">
         <h1>Filter Contacts App</h1>
-        <input type="text" />
+        <input type="text" onChange={handleText} />
         <div className="container">
-            {Data.map((Data) => <Contact Data={Data} />)}
+           {Data.filter((element) => {
+            if(text === ""){
+                return element
+            } else if (
+                element.first_name.toLocaleLowerCase().includes(text.toLocaleLowerCase()) 
+            ) {
+                return element
+            }
+           }).map((contact) => <Contact contact={contact} />)}
         </div>
     </div>
   )
